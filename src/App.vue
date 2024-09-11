c37bd659d57157da370c1c48937c6807793d0e8a
vaebe <18137693952@163.com>
2024-09-11 21:15:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import WrapInput from "./components/WrapInput.vue";
+
+const inputText = ref('')
+</script>
+
+<template>
+  <WrapInput v-model="inputText"></WrapInput>
+
+  <div>
+    {{inputText}}
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/WrapInput.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const model = defineModel()
+</script>
+
+<template>
+  <el-input v-model="model" placeholder="Please input" />
+</template>
+
+<style lang='scss' scoped></style>
